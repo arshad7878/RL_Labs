d458e2ac51f777072f91f8420b517e3d18967ca3
arshad7878 <50857772+arshad7878@users.noreply.github.com>
2019-09-05 14:11:57 -0400

    Create myFirst_Code.py

---
+++ myFirst_Code.py
@@ -0,0 +1,8 @@
+char_name = "Arshad Iqbal"
+char_age = "30"
+print("Hello sir.")
+print("my name is " +char_name+ " , and I am " +char_age+ " years old")
+print ("I want to be a doctor in Cyber Security")
+print ("Academic department has declared that Mr. " +char_name+ " is eligible for admission in PhD" )
+char_name = "Sajid"
+print ("Academic department also declared that Mr. " +char_name+ " is not eligible for admission in PhD" )
